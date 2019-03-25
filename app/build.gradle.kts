4e13dec596651834d8634ef743ad823669abed4c
Natalie Frecka <natalie.frecka@accenture.com>
2019-03-25 11:09:08 -0500

    Switched the app gradle file to the kotlin dsl
    Signed-off-by: Robert Murdock <robert.f.murdock@accenture.com>

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        applicationId = "com.pillar.gizmogrokker"
+        minSdkVersion(28)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        val release by getting {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to arrayOf("*.jar"))))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.21")
+    implementation("androidx.appcompat:appcompat:1.0.0-beta01")
+    implementation("androidx.core:core-ktx:1.1.0-alpha05")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test:runner:1.1.0-alpha4")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.0-alpha4")
+}
