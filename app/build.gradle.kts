97bf2a4cffa1ddd75b5688386387d1d61aa7b8c7
Natalie Frecka <natalie.frecka@accenture.com>
2019-04-12 11:04:26 -0500

    Rebuilt device test to avoid repeated expensive setup
    Signed-off-by: Robert Murdock <robert.f.murdock@accenture.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,9 +52,9 @@ dependencies {
     testImplementation("org.mockito:mockito-core:2.24.5")
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.1.0")
     testImplementation("junit:junit:4.12")
-    testImplementation("com.zegreatrob.testmints:standard:+")
+    testImplementation("com.zegreatrob.testmints:standard:1.3.2")
 
-    androidTestImplementation("com.zegreatrob.testmints:standard:+")
+    androidTestImplementation("com.zegreatrob.testmints:standard:1.3.2")
 
     androidTestImplementation("androidx.test:runner:1.1.2-alpha02")
 
--- app/src/androidTest/java/com/pillar/gizmogrokker/instrumented/BluetoothDeviceTest.kt
+++ app/src/androidTest/java/com/pillar/gizmogrokker/instrumented/BluetoothDeviceTest.kt
@@ -14,11 +14,13 @@ import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import com.pillar.gizmogrokker.*
+import com.pillar.gizmogrokker.BloothDevice
+import com.pillar.gizmogrokker.R
 import com.pillar.gizmogrokker.detail.DeviceDetailActivity
-import com.pillar.gizmogrokker.list.DeviceListFragment
 import com.pillar.gizmogrokker.list.DeviceListActivity
-import com.schibsted.spain.barista.interaction.PermissionGranter
+import com.pillar.gizmogrokker.list.DeviceListFragment
+import com.schibsted.spain.barista.interaction.PermissionGranter.allowPermissionsIfNeeded
+import com.zegreatrob.testmints.setup
 import kotlinx.android.synthetic.main.device_list_activity.*
 import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.Matchers.equalTo
@@ -30,44 +32,44 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class BluetoothDeviceTest {
+
     @get:Rule
     val activityRule = IntentsTestRule(DeviceListActivity::class.java)
+
+    private val findDevicesButton get() = onView(withId(R.id.findDevices))
+
+    private val deviceList get() = onView(withId(R.id.fragment))
+
+    private val deviceListFragment get() = activityRule.activity.fragment as DeviceListFragment
 
     @Before
     fun setUp() {
         assumeTrue(BluetoothAdapter.getDefaultAdapter() != null)
-
-        onView(withId(R.id.findDevices)).perform(click())
-
-        PermissionGranter.allowPermissionsIfNeeded(Manifest.permission.ACCESS_FINE_LOCATION)
-
-        deviceListFragment().run {
-            waitUntil(30_000) { deviceList.isNotEmpty() }
-        }
     }
 
     @Test
-    fun successfullyFindsAtLeastOneBluetoothDevice() {
-        onView(withId(R.id.fragment)).check(matches(hasMinimumChildCount(1)))
-    }
+    fun canFindBluetoothDevicesAndShowDetails(): Unit = setup(object {}) exercise {
+        findDevicesButton.perform(click())
+        ifPermissionBlockedGrantThem()
+        waitToFindDevices()
+    } verify {
+        deviceList.check(matches(hasMinimumChildCount(1)))
 
-    @Test
-    fun whenRecyclerViewItemIsClickedDeviceDetailActivityShows() {
-        onView(withId(R.id.fragment))
-            .perform(
-                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
-                    0,
-                    click()
-                )
-            )
-
+        nextStageOfTest()
+    } exercise {
+        deviceList.perform(clickOnItemAtIndex(0))
+    } verify {
         intended(hasComponent(DeviceDetailActivity::class.java.name))
         intended(
-            hasExtra(equalTo("device"), instanceOf<BloothDevice>(BloothDevice::class.java))
+            hasExtra(equalTo("device"), instanceOf<BloothDevice>())
         )
     }
 
-    private fun deviceListFragment() = activityRule.activity.fragment as DeviceListFragment
+    private fun ifPermissionBlockedGrantThem() {
+        allowPermissionsIfNeeded(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    private fun duration(start: Long) = System.currentTimeMillis() - start
 
     private fun waitUntil(waitTime: Int, shouldContinue: () -> Boolean) {
         val start = System.currentTimeMillis()
@@ -76,5 +78,19 @@ class BluetoothDeviceTest {
         }
     }
 
-    private fun duration(start: Long) = System.currentTimeMillis() - start
+    private fun waitToFindDevices() {
+        deviceListFragment.run {
+            waitUntil(30_000) { deviceList.isNotEmpty() }
+        }
+    }
+
+    private fun clickOnItemAtIndex(position: Int) =
+        RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+            position,
+            click()
+         )
+
+    private inline fun <reified T> instanceOf() = instanceOf<T>(T::class.java)
 }
+
+fun <C : Any> C.nextStageOfTest() = setup(this)
